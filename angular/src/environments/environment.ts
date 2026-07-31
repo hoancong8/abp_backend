9b9504ecab019b1b69c8f98ac1bc3ad3de77654d
Nguyễn Công Hoan <conghoan1182004@gmail.com>
2026-07-31 09:38:24 +0700

    Add development environment configs for local debugging without affecting production VPS

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,12 +3,12 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'http://localhost:4200';
 
 const oAuthConfig = {
-  issuer: 'https://localhost:44346/',
+  issuer: 'http://localhost:8080/',
   redirectUri: baseUrl,
   clientId: 'AbpSolution1_App',
   responseType: 'code',
   scope: 'offline_access AbpSolution1',
-  requireHttps: true,
+  requireHttps: false,
 };
 
 export const environment = {
@@ -20,7 +20,7 @@ export const environment = {
   oAuthConfig,
   apis: {
     default: {
-      url: 'https://localhost:44346',
+      url: 'http://localhost:8080',
       rootNamespace: 'AbpSolution1',
     },
     AbpAccountPublic: {
